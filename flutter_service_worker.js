5b05e3afa1a1c97a6b1a00b5d7ba7a0090b6056b
JleMyP <timtim96@bk.ru>
2021-04-07 22:04:50 +0000

    Deploying to gh-pages from @ JleMyP/skill-manager-client@74af32cb457d8d2ccad8208c9a29f935c3024ee5 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "manifest.json": "da2abbf13082f0ec764013abceece6cb",
+  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"assets/NOTICES": "8d44c9743ed8525df63a68f9e16c9e5f",
-"assets/assets/github-logo.svg": "b53ff55e09a092f9f0cbbf74af01b715",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"manifest.json": "da2abbf13082f0ec764013abceece6cb",
+"index.html": "d7e443fd7b3c7f94cec3386ff3f1dfaa",
+"/": "d7e443fd7b3c7f94cec3386ff3f1dfaa",
+"assets/NOTICES": "fe50be195818b3b94539be4894ae1174",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/AssetManifest.json": "da4c5b4c1747117afe7e58502d2638dc",
+"assets/assets/github-logo.svg": "b53ff55e09a092f9f0cbbf74af01b715",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
-"index.html": "c6b87960fc2dedeae44e29b80adc9201",
-"/": "c6b87960fc2dedeae44e29b80adc9201",
-"main.dart.js": "90f8a663dbb2791137b5c6bef751881d",
-"version.json": "d6950cc0e0bb0ac2451f5d50b0af2551",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796"
+"main.dart.js": "4f6e43e195ca15d8dc2b23ab029bd652",
+"version.json": "d6950cc0e0bb0ac2451f5d50b0af2551"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -33,7 +33,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
